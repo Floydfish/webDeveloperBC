69b83b8afbd9e5a48c6ea2ff1805195f9d13e7d9
carlosadap <38674510+Floydfish@users.noreply.github.com>
2020-08-26 12:56:32 +0200

    Moved files

--- javaScript/ageCalculator/script.js
+++ javaScript/ageCalculator/script.js
@@ -2,4 +2,4 @@ const ageYears = prompt("What is your age?")
 const ageDays = ageYears*365
 
 console.log("Your age in days is: " + ageDays)
-document.write("Your age in days is: " + ageDays)
+document.write("<h1>Your age in days is: " + ageDays + "</h1>")
