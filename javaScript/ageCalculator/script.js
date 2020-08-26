dd35f1624d4bd6841364c34b5a6295146845e1c4
carlosadap <38674510+Floydfish@users.noreply.github.com>
2020-08-26 12:32:32 +0200

    add document.write

--- javaScript/ageCalculator/script.js
+++ javaScript/ageCalculator/script.js
@@ -2,3 +2,4 @@ const ageYears = prompt("What is your age?")
 const ageDays = ageYears*365
 
 console.log("Your age in days is: " + ageDays)
+document.write("Your age in days is: " + ageDays)
--- javaScript/stalkerExercise/script.js
+++ javaScript/stalkerExercise/script.js
@@ -2,4 +2,4 @@ const fName = prompt("What's your first name?");
 const lName = prompt("What's your last name?");
 const age = prompt ("What's your age?")
 
-console.log("Your name is: " + fName + lName + " and your age is: " + age + " years old");
+console.log("Your name is: " + fName + " " + lName + " and your age is: " + age + " years old");
