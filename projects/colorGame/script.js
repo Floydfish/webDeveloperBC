b4aa3b7757d92f2076829c2eae54060683561f1e
Carlos Pinheiro <carlosadap@gmail.com>
2020-09-02 22:28:30 +0200

    refactoring a bit

--- projects/colorGame/script.js
+++ projects/colorGame/script.js
@@ -1,16 +1,10 @@
-let colors = [
-  "rgb(255, 0, 0)",
-  "rgb(255, 255, 0)",
-  "rgb(0, 255, 0)",
-  "rgb(0, 255, 255)",
-  "rgb(0, 0, 255)",
-  "rgb(255, 0, 255)",
-]
+let colors = generateRandomColors(6);
 
 const squares = document.querySelectorAll(".square");
 const pickedColor = pickColor();
 const colorDisplay = document.querySelector("#colorDisplay");
 const messageDisplay = document.querySelector("#message");
+const h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -24,6 +18,7 @@ for (let i = 0; i < squares.length; i++ ) {
     if (clickedColor === pickedColor) {
       messageDisplay.textContent = "CORRECT!"
       changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
       this.style.backgroundColor = "#232323"
       messageDisplay.textContent = "Try Again"
@@ -33,14 +28,39 @@ for (let i = 0; i < squares.length; i++ ) {
 
 function changeColors(color) {
   // Loop through all squares
-  for (var i = 0; i < colors.length; i++) {
+  for (let i = 0; i < colors.length; i++) {
     // change each color to match given color
     squares[i].style.backgroundColor = color;
   }
 };
 
 function pickColor() {
-  const random = Math.floor(Math.random() * colors.length);
+  const random = randomNum(colors.length);
   return colors[random]
-  // Math.floor(Math.random()*256)
+}
+
+function generateRandomColors(num) {
+  // make an array
+  const array = [];
+  //repeat num times
+  for (let i = 0; i < num; i++) {
+    //get random random colors and push into array
+    array.push(randomColor())
+  };
+  //return that array
+  return array;
+}
+
+function randomColor() {
+  // pick a "red" from 0 to 255
+  const r = randomNum(256)
+  // pick a "green" from 0 to 255
+  const g = randomNum(256)
+  // pick a "blue" from 0 to 255
+  const b = randomNum(256)
+  return `rgb(${r}, ${g}, ${b})`
+}
+
+function randomNum(n) {
+  return Math.floor(Math.random() * n)
 }
