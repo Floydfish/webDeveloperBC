cbc59db808b049a0a17d07674d9f3fe7f1ae031b
Carlos Pinheiro <carlosadap@gmail.com>
2020-09-16 16:50:02 +0200

    moved js out and .attr .last

---
+++ jQuery/methods/script1.js
@@ -0,0 +1,4 @@
+$("h1").text("New Text!!!");
+// $("li").text("Rusty, Colt's god, is adorable");
+$("ul").html("<li>I hacked your UL</li><li>I hacked your li</li>");
+$("li:first-of-type").html("<a href='www.google.com'>Click ME to go to GOOGLE</a>");
