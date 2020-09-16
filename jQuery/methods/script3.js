a641897cd2902db75c3493c1048a1a9fe49e9981
Carlos Pinheiro <carlosadap@gmail.com>
2020-09-16 18:07:33 +0200

    .add .remove .toggle

---
+++ jQuery/methods/script3.js
@@ -0,0 +1,10 @@
+$("h1").addClass("correct");
+$("h1").removeClass("correct");
+
+// $("li").addClass("wrong");
+// $("li").addClass("correct");
+$("li").toggleClass("correct");
+$("li").toggleClass("correct");
+
+$("li").first().toggleClass("done");
+$("li").toggleClass("done");
