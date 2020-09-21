2081f6089ec5ed4cd521f8837facaca952ccc73b
carlosadap <38674510+Floydfish@users.noreply.github.com>
2020-09-21 21:02:23 +0200

    removing items

--- projects/jQueryTodo/assets/js/todos.js
+++ projects/jQueryTodo/assets/js/todos.js
@@ -1,4 +1,12 @@
 // Check Off Specific Todos By Cliking
 $("li").click(function(){
-  $(this).toggleClass("completed")
+  $(this).toggleClass("completed");
+})
+
+//Click on X to delete todo
+$("span").click(function(e){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove(); //different this, this is the this.parent
+  });
+  e.stopPropagation();
 })
