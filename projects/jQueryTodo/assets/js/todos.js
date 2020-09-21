d6b89e11827b0a3e75bce103e2611b9fbf3db2c6
carlosadap <38674510+Floydfish@users.noreply.github.com>
2020-09-21 21:15:22 +0200

    Changing click for on, adding the listener to the parent

--- projects/jQueryTodo/assets/js/todos.js
+++ projects/jQueryTodo/assets/js/todos.js
@@ -1,12 +1,23 @@
 // Check Off Specific Todos By Cliking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 })
 
 //Click on X to delete todo
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
   $(this).parent().fadeOut(500, function(){
     $(this).remove(); //different this, this is the this.parent
   });
   e.stopPropagation();
-})
+});
+
+//adding items
+$("input[type='text']").keypress(function(e) {
+  if (e.which === 13) {
+    //grabbing new todo text
+    const todoText = $(this).val();
+    //clearing the input
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append(`<li> <span>X</span> ${todoText}</li>`)
+}})
