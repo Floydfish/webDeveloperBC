8cd06c93100019b9c567887050b0022c2cc35527
carlosadap <38674510+Floydfish@users.noreply.github.com>
2020-09-21 20:50:33 +0200

    toggleClass

--- projects/jQueryTodo/assets/js/todos.js
+++ projects/jQueryTodo/assets/js/todos.js
@@ -1,13 +1,4 @@
 // Check Off Specific Todos By Cliking
 $("li").click(function(){
-  if ($(this).css("color") === "gray") {
-    $(this).css({
-      textDecoration: "line-through",
-      color: "gray",})
-  } else {
-    $(this).css({
-      textDecoration: "line-through",
-      color: "gray",
-    })
-  }
+  $(this).toggleClass("completed")
 })
