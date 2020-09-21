070f78a040a14cc8af62c1f8f3f7804eb9f63cf9
Carlos Pinheiro <carlosadap@gmail.com>
2020-09-21 17:21:06 +0200

    Fix bugs

--- projects/jQueryTodo/assets/js/todos.js
+++ projects/jQueryTodo/assets/js/todos.js
@@ -1,1 +1,13 @@
-alert("Connected")
+// Check Off Specific Todos By Cliking
+$("li").click(function(){
+  if ($(this).css("color") === "gray") {
+    $(this).css({
+      textDecoration: "line-through",
+      color: "gray",})
+  } else {
+    $(this).css({
+      textDecoration: "line-through",
+      color: "gray",
+    })
+  }
+})
