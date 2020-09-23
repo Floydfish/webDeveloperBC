f64ae3e9f82789b14785605abb2a731a47b018ce
carlosadap <38674510+Floydfish@users.noreply.github.com>
2020-09-23 17:58:07 +0200

    cloning circles

--- projects/patatapClone/js/myScript.js
+++ projects/patatapClone/js/myScript.js
@@ -1,16 +1,27 @@
-// Create a Paper.js Path to draw a line into it:
-var path = new Path();
-// Give the stroke a color
-path.strokeColor = 'black';
-var start = new Point(100, 100);
-// Move to start and draw a line from there
-path.moveTo(start);
-// Note the plus operator on Point objects.
-// PaperScript does that for us, and much more!
-path.lineTo(start + [ 100, -50 ]);
+// var myCircle1 = new Path.Circle(new Point(100, 70), 10);
+// myCircle1.fillColor = 'black';
 
-var myCircle1 = new Path.Circle(new Point(100, 70), 10);
-myCircle1.fillColor = 'black';
+// var myCircle2 = new Path.Circle(new Point(50, 50), 300);
+// myCircle2.fillColor = 'purple';
 
-var myCircle2 = new Path.Circle(new Point(50, 50), 300);
-myCircle2.fillColor = 'purple';
+var circle = new Path.Circle({
+  center: [0, 0],
+  radius: 10,
+  fillColor: 'purple'
+});
+
+// for (var i = 0; i < 1001; i += 100) {
+//   for (var j = 0; j < 1001; j += 100) {
+//     var copy = circle.clone();
+//     copy.position.x = i;
+//     copy.position.y = j;
+//   }
+// }
+
+for (var i = 0; i < 10; i++) {
+  for (var j = 0; j < 10; j++) {
+    var copy = circle.clone();
+    copy.position.x += i * 100
+    copy.position.y += j * 100
+  }
+}
