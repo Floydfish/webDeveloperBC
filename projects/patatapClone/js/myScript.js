8bd8b07a9da75950cb66613ea4d947d89e79dab1
Carlos Pinheiro <carlosadap@gmail.com>
2020-09-24 17:28:06 +0200

    making it an object

--- projects/patatapClone/js/myScript.js
+++ projects/patatapClone/js/myScript.js
@@ -1,56 +1,32 @@
-// var myCircle1 = new Path.Circle(new Point(100, 70), 10);
-// myCircle1.fillColor = 'black';
-
-// var myCircle2 = new Path.Circle(new Point(50, 50), 300);
-// myCircle2.fillColor = 'purple';
-
-// var circle = new Path.Circle({
-//   center: [0, 0],
-//   radius: 10,
-//   fillColor: 'purple'
-// });
-
-// for (var i = 0; i < 1001; i += 100) {
-//   for (var j = 0; j < 1001; j += 100) {
-//     var copy = circle.clone();
-//     copy.position.x = i;
-//     copy.position.y = j;
-//   }
-// }
-
-// for (var i = 0; i < 10; i++) {
-//   for (var j = 0; j < 10; j++) {
-//     var copy = circle.clone();
-//     copy.position.x += i * 100
-//     copy.position.y += j * 100
-//   }
-// }
-
-// for (var x = 0; x < 1000; x += 100) {
-//   for (var y = 0; y < 1000; y += 100) {
-//     new Path.Circle(new Point(x, y), 10).fillColor = "purple";
-//   }
-// }
-
 var circles = [];
 
+var keyData = {
+  a: {
+    color: "purple",
+    sound: new Howl({
+      src: ['./sounds/bubbles.mp3']
+    })
+  },
+  b: {
+    color: "green",
+    sound: new Howl({
+      src: ['./sounds/clay.mp3']
+    })
+  },
+  c: {
+    color: "brown",
+    sound: new Howl({
+      src: ['./sounds/confetti.mp3']
+    })
+  },
+};
+
 function onKeyDown(event) {
-  // if key is "a"
-  if (event.key === "a") {
-    //play sound1
-    sound1.play();
-  }
-  // if key is "s"
-    if (event.key === "b") {
-      //play sound2  
-      sound2.play();
-    }
   var maxPoint = new Point(view.size.width, view.size.height);
   var randomPoint = Point.random();
   var point = maxPoint * randomPoint;
   var newCircle = new Path.Circle(point, 500);
-  
-  newCircle.fillColor = "orange";
+  newCircle.fillColor = keyData[event.key].color
 
   circles.push(newCircle);
 }
@@ -63,9 +39,7 @@ function onFrame(event) {
 }
 
 //Adding sounds
-var sound1 = new Howl({
-  src: ['./sounds/bubbles.mp3']
-});
+
 
 var sound2 = new Howl({
   src: ['./sounds/clay.mp3']
