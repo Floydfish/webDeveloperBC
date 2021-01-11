e22bfe16d7f45aa7d9fa5f87ed68bdd6cef08875
Carlos Pinheiro <carlosadap@gmail.com>
2021-01-11 12:32:39 +0100

    fixing the winning logic

--- js/scoreKeeper/app.js
+++ js/scoreKeeper/app.js
@@ -3,26 +3,44 @@ const p2Score = document.querySelector("#p2Score");
 const btnP1 = document.querySelector("#btnP1");
 const btnP2 = document.querySelector("#btnP2");
 const btnR = document.querySelector("#btnR");
-const numberMatches = document.querySelector("#numberMatches");
+const playTo = document.querySelector("#playTo");
 
 let currP1Score = 0;
 let currP2Score = 0;
+let winningScore = 3;
+let isGameOver = false;
 
 btnP1.addEventListener('click', function (e) {
-    currP1Score++
-    p1Score.innerText = currP1Score;
-    if (currP1Score === numberMatches.value) {
-        p1Score.innerText.style.color = "green";
+    if (!isGameOver) {
+        currP1Score++
+        if (currP1Score === winningScore) {
+            isGameOver = true;
+        }
+        p1Score.innerText = currP1Score;
     }
 });
 
 btnP2.addEventListener('click', function (e) {
-    currP2Score++
-    p2Score.innerText = currP2Score;
+    if (!isGameOver) {
+        currP2Score++
+        if (currP2Score === winningScore) {
+            isGameOver = true;
+        }
+        p2Score.innerText = currP2Score;
+    }
 });
 
-btnR.addEventListener('click', function(e) {
+
+btnR.addEventListener('click', reset);
+
+playTo.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+function reset() {
+    isGameOver = false;
     currP1Score = currP2Score = 0;
     p1Score.innerText = p2Score.innerText = currP1Score;
-});
+}
 
